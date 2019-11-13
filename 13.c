4251e16b0214ad39d041077a24ffac8f16c48546
mathmh <56640194+mathmh@users.noreply.github.com>
2019-11-13 09:13:32 -0300

    projeto_13

---
+++ 13.c
@@ -0,0 +1,18 @@
+    int check3x3() {
+
+        int lin = 0, col = 0, cont = 0;
+        if(lin == lin++){
+            lin++;
+            cont_lin ++;
+            if(cont_lin == 3){
+                if(col == col++){
+                    col++;
+                    cont_col++;
+                        if(cont_col == 3){
+                            return 1;
+                        }
+                }
+            }
+        }
+        return 0
+    }
